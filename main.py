b87cfbfb300f09ffb0017646208583981a9cea04
Nikitapasalski <pasnikita2106@gmail.com>
2025-09-06 12:02:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+age = 25
+mom_age = 40
+height = 1.75
+name = "Олександр"
+mom_name = "Марія"
+favorite_fruits = ["Яблуко", "Банан", "Виноград"]
+im_student = True
+cat = {"name":"Baton","age": "3"}
+mnoz = {5,4,6}
+birth_date = (2, 10, 2008)
+print("type", type(age))
+print("type", type(mom_age))
+print("name", type(name))
+print("type", type(height))
+print("type", type(mom_name))
+print("type", type(favorite_fruits))
+print("type", type(im_student))
+print("type", type(cat))
+print("type", type(mnoz))
+print("type", type(birth_date))
+print(age)
+print(mom_age)
+print(name)
+print(height)
+print(mom_name)
+print(favorite_fruits)
+print(im_student)
+print(cat)
+print(mnoz)
+print(birth_date)
